750bfff873348a1723cd0d706bd61e83279003ee
MoonMinHyuk1 <moon981018@gmail.com>
2025-01-08 22:37:34 +0900

    initial

---
+++ repeat/src/main/java/jpa/study/repeat/RepeatApplication.java
@@ -0,0 +1,13 @@
+package jpa.study.repeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatApplication.class, args);
+	}
+
+}
