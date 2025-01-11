7c9e07849cc01f03376a6f884577dabd7a222eba
MoonMinHyuk1 <moon981018@gmail.com>
2025-01-12 00:16:14 +0900

    chapter 04

--- repeat/src/test/java/jpa/study/repeat/RepeatApplicationTests.java
+++ repeat/src/test/java/jpa/study/repeat/RepeatApplicationTests.java
@@ -1,5 +1,9 @@
 package jpa.study.repeat;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,30 @@ class RepeatApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void logic() {
+		//엔티티 매니저 팩토리 생성
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+		EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+
+		EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+
+		try {
+
+			tx.begin(); //트랜잭션 시작
+			//TODO 비즈니스 로직
+			tx.commit();//트랜잭션 커밋
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback(); //트랜잭션 롤백
+		} finally {
+			em.close(); //엔티티 매니저 종료
+		}
+
+		emf.close(); //엔티티 매니저 팩토리 종료
 	}
 
 }
